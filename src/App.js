a18e00d8cc8acc1f591c6dd65ddb7d21bdd8d38a
Claudia Dias <claudiamld@outlook.com>
2022-04-26 09:45:03 -0300

    feat: toastContainer added to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import { Routes } from "./routes";
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify';
 
 function App() {
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes />
     </div>
   );
